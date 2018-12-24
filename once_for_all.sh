4850e814780fa5fb5d9c5fa43c1995a2d5140518
gaunthan <gaunthanhuang@foxmail.com>
2018-12-24 21:32:49 +0800

    Once for all script.

---
+++ once_for_all.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+# Run this script for calling all the other scripts
+
+# Install curl for running there scripts online.
+sudo apt install curl -y
+
+# Run all the scripts, respectively.
+curl -sLf https://raw.githubusercontent.com/gaunthan/Oh-My-Ubuntu/master/setup_system.sh | bash
+curl -sLf https://raw.githubusercontent.com/gaunthan/Oh-My-Ubuntu/master/enable_chinese_input.sh | bash
+curl -sLf https://raw.githubusercontent.com/gaunthan/Oh-My-Ubuntu/master/beautify-gnome-shell.sh | bash
