716cf2f29b951d228e7abb110234ee952093ab23
gaunthan <gaunthanhuang@foxmail.com>
2018-12-24 21:11:49 +0800

    Add new function: changing wallpapers, beautifying dock.

--- beautify-gnome-shell.sh
+++ beautify-gnome-shell.sh
@@ -18,6 +18,7 @@ echo "You may need to enable extensions manually."
 echo "If did, please run this script again after extension enabled, for configuration."
 
 gnome-shell-extension-tool -e user-theme
+gnome-shell-extension-tool -e dash-to-dock
 
 # Setup theme and icons
 install_packages \
@@ -32,6 +33,24 @@ gsettings set org.gnome.desktop.interface gtk-theme Arc-Dark
 gsettings set org.gnome.desktop.wm.preferences theme Arc-Dark
 gsettings set org.gnome.desktop.interface icon-theme Papirus
 gsettings set org.gnome.desktop.interface cursor-theme Breeze_Snow
+
+# Configure dash to dock
+# Replace 'set' to 'reset' to restore to default if needed.
+echo "Configuring gnome-shell-extension-dash-to-dock..."
+#gsettings set org.gnome.shell.extensions.dash-to-dock show-apps-at-top true
+gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-running-dots false
+gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-customize-running-dots false
+gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-shrink true
+gsettings set org.gnome.shell.extensions.dash-to-dock background-opacity 0
+gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 32
+gsettings set org.gnome.shell.extensions.dash-to-dock click-action minimize-or-overview
+gsettings set org.gnome.shell.extensions.dash-to-dock scroll-action cycle-windows
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
+gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
+
+# Configure wallpapers
+gsettings set org.gnome.desktop.background picture-uri 'file:///usr/share/backgrounds/Spices_in_Athens_by_Makis_Chourdakis.jpg'
+gsettings set org.gnome.desktop.screensaver picture-uri 'file:///usr/share/backgrounds/On_top_of_the_Rubihorn_by_Matthias_Niess.jpg'
 
 # Restart gnome-shell 
 echo "Restarting gnome-shell to flush the configurations..."
