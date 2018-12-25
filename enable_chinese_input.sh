f2da3edbd8506def8974a1a0340a2d218664b4a0
Gaunthan Huang <gauntHanHuang@foxmail.com>
2018-12-25 09:38:13 +0800

    Set default English status for input method

--- enable_chinese_input.sh
+++ enable_chinese_input.sh
@@ -10,8 +10,12 @@ im-config -n fcitx
 fcitx-autostart
 
 # Configure rime
+# 
+# Set default simple chinese input
 sed -i '7s/luna_pinyin/luna_pinyin_simp/' ~/.config/fcitx/rime/default.yaml
 sed -i '9s/luna_pinyin_simp/luna_pinyin/' ~/.config/fcitx/rime/default.yaml
+# Set default English status for input method
+sed -i '15s/0/1/' luna_pinyin_simp.schema.yaml
 
 echo "Now you can run fcitx-configtool to add rime manully."
 echo "After that, please log out to make effect."
