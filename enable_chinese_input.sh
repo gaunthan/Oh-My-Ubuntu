c18836aae2b7193f544cc4825d9d64314e2b5ecf
gaunthan <gaunthanhuang@foxmail.com>
2018-12-25 20:36:52 +0800

    Add command for changing fcitx's skin.

--- enable_chinese_input.sh
+++ enable_chinese_input.sh
@@ -9,11 +9,20 @@ sudo apt install fcitx fcitx-rime -y
 im-config -n fcitx
 fcitx-autostart
 
+# Change fcitx's skin to material
+curl -sLf https://raw.githubusercontent.com/gaunthan/fcitx-skin-collection/master/install-fcitx-skins.sh | bash
+SkinType=material
+sed -i 's/SkinType=.*/SkinType='"$SkinType"'/' conf/fcitx-classic-ui.config
+
+
 # Configure rime
 # 
+echo "Configuring chinese input method 'rime'..."
+
 # Set default simple chinese input
 sed -i '7s/luna_pinyin/luna_pinyin_simp/' ~/.config/fcitx/rime/default.yaml
 sed -i '9s/luna_pinyin_simp/luna_pinyin/' ~/.config/fcitx/rime/default.yaml
+
 # Set default English status for input method
 sed -i '15s/0/1/' luna_pinyin_simp.schema.yaml
 
